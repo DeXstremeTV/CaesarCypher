351ebc3a1cc6ab6e67f23a526672e71a7918ea6c
Ricardo <toonsytoons@gmail.com>
2024-06-27 10:20:00 +0100

    Casear cypher finished

---
+++ caesar_cypher.rb
@@ -0,0 +1,20 @@
+def caesar_cypher(string , shift)
+    cypher = []
+    input_array = string.downcase.chars.map! { |i| i.ord}
+    input_array.each do |j|
+        if j < 97 || j > 122
+            cypher.push(j)
+        elsif j - shift < 97
+            x = j - shift
+            y = 97 - x
+            cypher.push(123 - y)
+        else
+            cypher.push(j - shift)
+        end
+    end
+
+    cypher.map! { |k| k.chr }
+    cypher.join
+end
+
+caesar_cypher("What a String!" , 5)
